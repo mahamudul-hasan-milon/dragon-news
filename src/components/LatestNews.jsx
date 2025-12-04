8c79793081c8eb167ee4185c81b34be3130fb267
Md. Mahamudul Hasan Milon <155943998+mahamudul-hasan-milon@users.noreply.github.com>
2025-12-05 00:20:18 +0600

    added some component

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,13 @@
+const LatestNews = () => {
+  return (
+    <div className="flex gap-2 items-center bg-base-200 p-2">
+      <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+        voluptatibus?
+      </p>
+    </div>
+  );
+};
+
+export default LatestNews;
