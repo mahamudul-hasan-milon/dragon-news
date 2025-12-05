70fa2122f55b2c2c3f0ae3d9d6f464b7258709b0
Md. Mahamudul Hasan Milon <155943998+mahamudul-hasan-milon@users.noreply.github.com>
2025-12-05 10:06:38 +0600

    added marquee

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,10 +1,13 @@
+import "../components/Marquee.css";
+
 const LatestNews = () => {
   return (
     <div className="flex gap-2 items-center bg-base-200 p-2">
       <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
-        voluptatibus?
+      <p className="animate-marquee whitespace-nowrap">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        repellendus dolore natus cupiditate qui autem temporibus in velit error
+        odit.
       </p>
     </div>
   );
